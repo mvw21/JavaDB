ccc4b6730edfe7464086c4755dc315a095924766
mvw21 <53233579+mvw21@users.noreply.github.com>
2020-01-08 21:31:30 +0200

    Update

--- Data Definition And Data Types - Lab.sql
+++ Data Definition And Data Types - Lab.sql
@@ -1,4 +1,4 @@
-//01.Create Tables
+/*01.Create Tables*/
 CREATE TABLE employees(
 id INT(11) PRIMARY KEY AUTO_INCREMENT,
 first_name VARCHAR(45) NOT NULL,
@@ -14,20 +14,20 @@ name VARCHAR(45) NOT NULL,
 category_id VARCHAR(45) NOT NULL
 );
 
-//02.Insert info
+/*02.Insert info*/
 INSERT INTO employees (first_name,last_name) VALUES ('Pesho','Peshov');
 INSERT INTO employees (first_name,last_name) VALUES ('Gosho','Goshov');
 INSERT INTO employees (first_name,last_name) VALUES ('Tosho','Toshov');
 
-//03.Alter tables
+/*03.Alter tables*/
 ALTER TABLE employees
 ADD middle_name VARCHAR(255);
 
-//04.Adding Constraints
+/*04.Adding Constraints*/
 ALTER TABLE products
 ADD CONSTRAINT FK_category_id
 FOREIGN KEY (category_id) REFERENCES categories(id);
 
-//05.Modifying Columns
+/*05.Modifying Columns*/
 ALTER TABLE employees
 MODIFY COLUMN middle_name VARCHAR(100);

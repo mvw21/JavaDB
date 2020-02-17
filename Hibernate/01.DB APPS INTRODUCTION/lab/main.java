0b29a4e2c88f8b21901435bc8461e210f844acb7
mvw21 <53233579+mvw21@users.noreply.github.com>
2020-02-17 22:02:57 +0200

    adding more exercises

--- Hibernate/01.DB APPS INTRODUCTION/lab/main.java
+++ Hibernate/01.DB APPS INTRODUCTION/lab/main.java
@@ -10,7 +10,7 @@ import java.sql.*;
 
 public class Main {
     private static final String CONNECT_STRING = "jdbc:mysql://localhost:3306/";
-    private static final String DATABASE_NAME = "minions_db?serverTimezone=UTC";
+    private static final String DATABASE_NAME = "minions_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
     private static Connection connection;
     private static String query ;
@@ -21,22 +21,62 @@ public class Main {
 
         reader = new BufferedReader(new InputStreamReader(System.in));
 
+        System.out.print("Enter username default (root): ");
+        String user = reader.readLine();
+        user = user.equals("") ? "root" : user;
+        System.out.println();
+
+        System.out.print("Enter password default (empty):");
+        String password = reader.readLine().trim();
+        System.out.println();
+
         Properties props = new Properties();
-        props.setProperty("user", "root");
-        props.setProperty("password", "roo7");
+        props.setProperty("user", user);
+        props.setProperty("password", password);
 
 
         connection = DriverManager
                 .getConnection(CONNECT_STRING  + DATABASE_NAME, props);
 
         //02. Get Villains’ Names
-        getVillainsNamesAndCountOfMinions();
+//        getVillainsNamesAndCountOfMinions();
 
         //03. Get Minion Names
-        getMinionNamesEx(); 
+//        getMinionNamesEx();
+
+        //05. Change Town Names Casing
+        changeTownNamesCasing();
 
 
+    }
 
+    private static void changeTownNamesCasing() throws IOException, SQLException {
+        System.out.println("Enter a country:");
+        String countryToUpdate = reader.readLine();
+
+        statement = connection.prepareStatement("UPDATE towns SET name = UPPER(name) WHERE country = ? AND name NOT LIKE BINARY UPPER(name) ");
+        statement.setString(1, countryToUpdate);
+
+        int affectedRows = statement.executeUpdate();
+
+        if(affectedRows > 0){
+            statement = connection.prepareStatement("SELECT name FROM towns WHERE country = ?");
+            statement.setString(1, countryToUpdate);
+            ResultSet updatedCountriesRS = statement.executeQuery();
+
+            String result = "[";
+            while(updatedCountriesRS.next()){
+                result += updatedCountriesRS.getString("name") + ", ";
+            }
+            result = result.substring(0, result.length() - 2);
+            result += "]";
+
+            System.out.println(affectedRows + " town names were affected.");
+            System.out.println(result);
+        }
+        else{
+            System.out.println("No town names were affected");
+        }
 
     }
 
